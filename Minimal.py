48854dc6d67a5bca39265691f5601def0b442d26
Victor Reyes <haveon3@gmail.com>
2014-10-26 19:46:07 -0400

    Playing with route decorator

--- Minimal.py
+++ Minimal.py
@@ -3,11 +3,16 @@ from flask import Flask
 #First argument: name of the app's module of package
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Index Page"
+    
 #Decorate hello world with route
 #Route specifies the url route
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello World'
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False)
