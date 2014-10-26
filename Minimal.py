c51f3794979346cc53c1b08639422eb4500c0078
Victor Reyes <haveon3@gmail.com>
2014-10-26 19:59:02 -0400

    Variable names in route decorators

--- Minimal.py
+++ Minimal.py
@@ -6,12 +6,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Index Page"
-    
+
 #Decorate hello world with route
 #Route specifies the url route
 @app.route('/hello')
 def hello_world():
     return 'Hello World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' %username
+
+@app.route('/add/<float:num1>/<float:num2>')
+def addition(num1,num2):
+    #needs to return a str to print on page
+    return str(num1+num2)
 
 
 if __name__ == '__main__':
